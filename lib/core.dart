05fc91c6ea7c7467661bb8e9d0fc781eb4ae1136
Ruel Sison <rsjudge17@hotmail.com>
2021-03-30 18:48:05 +0800

    Added project files

---
+++ lib/core.dart
@@ -0,0 +1,49 @@
+library local_people_core;
+
+export './src/core/widgets/loading_widget.dart';
+export './src/core/widgets/custom_alert.dart';
+export './src/core/widgets/spacer_widget.dart';
+export './src/core/widgets/logo_widget.dart';
+
+export './src/core/local/localization.dart';
+
+export './src/core/configs/app_config.dart';
+export './src/core/configs/auth_config.dart';
+export './src/core/configs/theme_config.dart';
+export './src/core/enum/api_request_status.dart';
+export './src/core/enum/view_state.dart';
+
+export './src/core/utils/router.dart';
+export './src/core/utils/dialogs.dart';
+export './src/core/utils/scroll_behavior.dart';
+export './src/core/utils/validators.dart';
+export './src/core/utils/logging_setup.dart';
+
+/*import './src/core/bloc/bloc_event_state.dart';
+import './src/core/bloc/bloc_provider.dart';
+import './src/core/bloc/bloc_state_builder.dart';
+import './src/core/bloc/bloc_state_transform_base.dart';
+import './src/core/bloc/bloc_state_transform_builder.dart';*/
+
+//export 'src/splash/ui/views/splash_screen.dart';
+export './splash.dart';
+
+export './src/data/models/user_model.dart';
+export './src/data/models/auth_session_model.dart';
+export './src/data/models/auth_profile_model.dart';
+export './src/data/models/auth_model.dart';
+
+export './src/domain/entities/user.dart';
+export './src/domain/repositories/user_repository.dart';
+
+export './src/ui/blocs/login_bloc.dart';
+export './src/ui/views/login_screen.dart';
+//export './src/authentication/authentication.dart';
+
+export './src/data/datasources/user_data_source.dart';
+export './src/data/datasources/auth_data_source.dart';
+
+export './src/domain/repositories/authentication_repository.dart';
+export './src/domain/repositories/user_repository.dart';
+export './src/data/repositories/user_repository.dart';
+export './src/data/repositories/authentication_repository.dart';
