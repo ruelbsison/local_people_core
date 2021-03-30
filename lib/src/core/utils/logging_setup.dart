081e744bbde8d9414997ede53f4868829af17234
Ruel Sison <rsjudge17@hotmail.com>
2021-03-30 17:53:18 +0800

    Added utils

---
+++ lib/src/core/utils/logging_setup.dart
@@ -0,0 +1,8 @@
+import 'package:logging/logging.dart';
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+}
