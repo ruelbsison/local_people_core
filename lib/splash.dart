7f71ab841b2c77f2f036d14f828dd070c165bbbc
Ruel Sison <rsjudge17@hotmail.com>
2021-04-06 16:31:16 +0800

    Added Splash export file

---
+++ lib/splash.dart
@@ -0,0 +1,6 @@
+library local_people_core;
+
+export 'src/splash/ui/blocs/splash_bloc.dart';
+//export './src/ui/blocs/splash_event.dart';
+//export './src/ui/blocs/splash_state.dart';
+export 'src/splash/ui/views/splash_screen.dart';
