4465d7ebb503452a54639b05487cc1ee61ddce1d
Ruel Sison <rsjudge17@hotmail.com>
2021-04-06 16:30:27 +0800

    Added Auth export file

---
+++ lib/auth.dart
@@ -0,0 +1,10 @@
+library local_people_core;
+
+export './src/auth/domain/repositories/auth_repository.dart';
+
+export './src/auth/ui/blocs/authentication_bloc.dart';
+
+export './src/auth/data/models/auth_local_model.dart';
+export './src/auth/data/datasources/auth_local_data_source.dart';
+export './src/auth/data/repositories/auth_repository_impl.dart';
+
