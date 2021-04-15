f4dac174bd7a46500d0a3ca8063c08bc04774c7d
Ruel Sison <rsjudge17@hotmail.com>
2021-04-15 23:48:05 +0800

    Added Messages Module

---
+++ lib/messages.dart
@@ -0,0 +1,16 @@
+library local_people_core;
+
+export './src/messages/domain/entities/message.dart';
+export './src/messages/domain/entities/message_box.dart';
+
+export './src/messages/ui/widgets/text_message.dart';
+export './src/messages/ui/widgets/audio_message.dart';
+export './src/messages/ui/widgets/video_message.dart';
+export './src/messages/ui/widgets/message_input_field.dart';
+export './src/messages/ui/widgets/message_widget.dart';
+export './src/messages/ui/widgets/message_body.dart';
+export './src/messages/ui/widgets/message_box_card.dart';
+export './src/messages/ui/widgets/message_box_body.dart';
+
+export './src/messages/ui/views/messages_screen.dart';
+export './src/messages/ui/views/message_box_screen.dart';
---
+++ lib/src/core/configs/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kErrorColor = Color(0xFFF03738);
+const kDefaultPadding = 20.0;
